e2c218542339e2c40b01baeed4d9129b76728333
Joe Canero <caneroj1@tcnj.edu>
2015-07-03 19:43:14 -0400

    fixed counting of diff.

--- routes/bf.js
+++ routes/bf.js
@@ -10,10 +10,13 @@ exports.get_data = function(req, res) {
 
 exports.post_data = function(req, res) {
   var floatTemp = req.body['temp'];
+  var prevDiff = fridgeData['diff'];
   Object.keys(req.body).map(function(key, i) {
     req.body[key] = parseInt(req.body[key]);
   });
   fridgeData = merge(fridgeData, req.body);
   fridgeData['floatTemp'] = floatTemp;
+  if(typeof(req.body['diff']) != undefined)
+    fridgeData['diff'] = prevDiff + parseInt(req.body['diff']);
   res.json(fridgeData);
 };
